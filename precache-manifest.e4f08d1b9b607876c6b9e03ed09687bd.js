1d4a9bd727cd5b37bc9a9387a491634b8471a194
mitsurubil <mitsurubil@hotmail.com>
2019-10-11 21:41:59 +0100

    Updates

---
+++ precache-manifest.e4f08d1b9b607876c6b9e03ed09687bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4779de56fded9a8f4b5deb4ab0334e8f",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "fd24366d20dbf3ed8128",
+    "url": "/recipe-app/static/css/main.89c8183a.chunk.css"
+  },
+  {
+    "revision": "6c1b38f14a631cf2f1cd",
+    "url": "/recipe-app/static/js/2.c679f685.chunk.js"
+  },
+  {
+    "revision": "fd24366d20dbf3ed8128",
+    "url": "/recipe-app/static/js/main.f925734e.chunk.js"
+  },
+  {
+    "revision": "2cdc37c46ee7a3de3f17",
+    "url": "/recipe-app/static/js/runtime~main.2c4ead4e.js"
+  }
+]);
